ad78d4f07930fc4101f846dc8768c7756ef58761
Damien DE SOUSA <damien.desousa@smile.fr>
2022-07-04 10:14:43 +0200

    in progress

--- src/DadesGregwarCaptchaExtensionBundle.php
+++ src/DadesGregwarCaptchaExtensionBundle.php
@@ -1,11 +1,22 @@
 <?php
 
+/**
+ * Defines DadesGregwarCaptchaExtensionBundle class.
+ *
+ * @author Damien DE SOUSA
+ */
+
+declare(strict_types=1);
+
 namespace Dades\GregwarCaptchaExtensionBundle;
 
 use Dades\GregwarCaptchaExtensionBundle\DependencyInjection\DadesGregwarCaptchaExtensionExtension;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Defines the bundle.
+ */
 class DadesGregwarCaptchaExtensionBundle extends Bundle
 {
     public function getContainerExtension(): ExtensionInterface|null
--- src/DependencyInjection/DadesGregwarCaptchaExtensionExtension.php
+++ src/DependencyInjection/DadesGregwarCaptchaExtensionExtension.php
@@ -1,14 +1,29 @@
 <?php
 
+/**
+ * Defines DadesGregwarCaptchaExtensionExtension class.
+ *
+ * @author Damien DE SOUSA
+ */
+
+declare(strict_types=1);
+
 namespace Dades\GregwarCaptchaExtensionBundle\DependencyInjection;
 
+use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
+/**
+ * Loads config for the bundle.
+ */
 class DadesGregwarCaptchaExtensionExtension extends Extension
 {
+    /**
+     * @throws Exception
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
